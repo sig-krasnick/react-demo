afcd083ab7ef6cfb71cca66f6c67edda1a430be9
mkrasnick <matthew.krasnick@gmail.com>
2023-04-16 18:45:49 -0700

    Fix to NewPost

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Posts, { loader as postsLoader } from "./routes/Posts";
-import NewPost from "./routes/NewPost";
+import NewPost, { action as newPostAction } from "./routes/NewPost";
 import RootLayout from "./routes/RootLayout";
 import "./index.css";
 
@@ -16,7 +16,9 @@ const router = createBrowserRouter([
         path: "/",
         element: <Posts />,
         loader: postsLoader,
-        children: [{ path: "/new", element: <NewPost /> }],
+        children: [
+          { path: "/new", element: <NewPost />, action: newPostAction },
+        ],
       },
     ],
   },
